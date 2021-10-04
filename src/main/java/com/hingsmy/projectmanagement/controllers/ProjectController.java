c0242bf0ff9669c34464bd2c051ec651ad898186
hingsmy <hingsmy@gmail.com>
2021-10-04 13:17:04 +0800

    Navigation

--- src/main/java/com/hingsmy/projectmanagement/controllers/ProjectController.java
+++ src/main/java/com/hingsmy/projectmanagement/controllers/ProjectController.java
@@ -9,12 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
 
     @Autowired
     ProjectRepository proRepo;
+
+    @GetMapping
+    public String displayProjects(Model model) {
+        List<Project> projects = proRepo.findAll();
+        model.addAttribute("projects", projects);
+        return "projects/list-projects";
+    }
 
     @GetMapping("/new")
     public String displayProjectForm(Model model) {
