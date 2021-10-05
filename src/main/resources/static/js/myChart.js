4e6f0029bb54f82b249dea35f3ee46dba6a45c94
hingsmy <hingsmy@gmail.com>
2021-10-05 10:08:38 +0800

    Improve the homepage

--- src/main/resources/static/js/myChart.js
+++ src/main/resources/static/js/myChart.js
@@ -1,6 +1,26 @@
 // For a pie chart
-var myPieChart = new Chart(ctx, {
+const labels = [
+    'January',
+    'February',
+    'March'
+];
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: ['#3e95cd', '#8e5ea2', '#3cba9f'],
+        borderColor: 'rgb(255, 99, 132)',
+        data: [5, 2, 20],
+    }]
+};
+
+const config = {
     type: 'pie',
     data: data,
-    options: options
-});
+    options: {}
+};
+
+new Chart(
+    document.getElementById('myPieChart'),
+    config
+);
