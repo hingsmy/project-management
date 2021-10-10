cd9570a59c0058b1fc6e170f52d0b76f1cd09da5
hingsmy <hingsmy@gmail.com>
2021-10-10 22:41:51 +0800

    Spring AOP: joinPoint and Proceeding.JoinPoint with @Around

--- src/main/java/com/hingsmy/projectmanagement/logging/ApplicationLoggerAspect.java
+++ src/main/java/com/hingsmy/projectmanagement/logging/ApplicationLoggerAspect.java
@@ -1,11 +1,14 @@
 package com.hingsmy.projectmanagement.logging;
 
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 @Aspect
@@ -13,13 +16,35 @@ public class ApplicationLoggerAspect {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    @Pointcut("within(com.hingsmy.projectmanagement.controllers..*)" + "|| within(com.hingsmy.projectmanagement.dao..*)")
+    @Pointcut("within(com.hingsmy.projectmanagement.controllers..*)")
     public void definePackagePointcuts() {
         // empty method just to name the location specified in the pointcut
     }
 
-    @After("definePackagePointcuts()")
-    public void log() {
-        log.debug(" ---------------------- ");
+    @Around("definePackagePointcuts()")
+    public Object logAround(ProceedingJoinPoint joinPoint) {
+        log.debug("\n\n\n\n");
+        log.debug("************* Before Method Execution *************** \n {}. {} with argument[s] = {}",
+                joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(),
+                Arrays.toString(joinPoint.getArgs())
+        );
+        log.debug("______________________________________________\n\n\n");
+
+        Object o = null;
+        try {
+            o = joinPoint.proceed();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+        log.debug("************* After Method Execution *************** \n {}. {} with argument[s] = {}",
+                joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(),
+                Arrays.toString(joinPoint.getArgs())
+        );
+        log.debug("______________________________________________\n\n\n");
+
+        return o;
     }
 }
