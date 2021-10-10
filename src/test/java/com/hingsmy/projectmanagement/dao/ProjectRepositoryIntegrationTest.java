9bb3bfa4d0b1dfef5a6fa96671ef19ababa7c88e
hingsmy <hingsmy@gmail.com>
2021-10-10 20:21:27 +0800

    use @SpringBootTest annotation correctly

---
+++ src/test/java/com/hingsmy/projectmanagement/dao/ProjectRepositoryIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.hingsmy.projectmanagement.dao;
+
+import com.hingsmy.projectmanagement.entities.Project;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ProjectRepositoryIntegrationTest {
+
+    @Autowired
+    ProjectRepository proRepo;
+
+    @Test
+    public void ifNewProjectSaved_thenSuccess() {
+        Project newProject = new Project("New Test Project", "COMPLETE", "Test Description");
+        proRepo.save(newProject);
+
+        assertEquals(1, proRepo.findAll().size());
+    }
+}
