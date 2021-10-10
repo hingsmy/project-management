e2b956bc6befbf28a4fa48ced8490b390e81f374
hingsmy <hingsmy@gmail.com>
2021-10-10 11:14:31 +0800

    Read environment variables in property configuration file

--- src/main/java/com/hingsmy/projectmanagement/controllers/HomeController.java
+++ src/main/java/com/hingsmy/projectmanagement/controllers/HomeController.java
@@ -8,6 +8,7 @@ import com.hingsmy.projectmanagement.dto.ChartData;
 import com.hingsmy.projectmanagement.dto.EmployeeProject;
 import com.hingsmy.projectmanagement.entities.Project;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,9 @@ import java.util.Map;
 @Controller
 public class HomeController {
 
+    @Value("${version}")
+    private String ver;
+
     @Autowired
     ProjectRepository proRepo;
 
@@ -27,6 +31,8 @@ public class HomeController {
 
     @GetMapping("/")
     public String displayHome(Model model) throws JsonProcessingException {
+
+        model.addAttribute("versionNumber", ver);
 
         Map<String, Object> map = new HashMap<>();
 
