22c75f318374f690ad134f4812b77608b801d10c
hingsmy <hingsmy@gmail.com>
2021-10-11 11:01:43 +0800

    Spring security: postgres database with JDBC authorization and authentication

--- src/main/java/com/hingsmy/projectmanagement/security/SecurityConfiguration.java
+++ src/main/java/com/hingsmy/projectmanagement/security/SecurityConfiguration.java
@@ -1,14 +1,12 @@
 package com.hingsmy.projectmanagement.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -20,44 +18,34 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
-                .withDefaultSchema()
-                .withUser("myuser")
-                .password("1234")
-                .roles("USER")
-                .and()
-                .withUser("hings")
-                .password("1234")
-                .roles("USER")
-                .and()
-                .withUser("managerUser")
-                .password("1234")
-                .roles("ADMIN");
+                .usersByUsernameQuery(
+                        "select username, password, enabled " +
+                        "from user_accounts where username = ?"
+                )
+                .authoritiesByUsernameQuery(
+                        "select username, role " +
+                        "from user_accounts where username = ?"
+                )
+                .passwordEncoder(bCryptPasswordEncoder);
     }
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.authorizeRequests()
-                .antMatchers("/projects/new")
-                .hasRole("ADMIN")
-                .antMatchers("/employees/new")
-                .hasRole("ADMIN")
-                .antMatchers("/h2-console/**")
-                .permitAll()
-                .antMatchers("/")
-                .authenticated()
-                .and()
-                .formLogin();
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
+                .antMatchers("/projects/new").hasRole("ADMIN")
+                .antMatchers("/projects/save").hasRole("ADMIN")
+                .antMatchers("/employees/new").hasRole("ADMIN")
+                .antMatchers("/employees/save").hasRole("ADMIN")
+                .antMatchers("/", "/**").permitAll()
+                .and().formLogin();
+
     }
 }
